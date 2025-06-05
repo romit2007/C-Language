419a376fcf611e9a026e96f56d96fa1e9314bbd5
romit2007 <romitvaghela2007@gmail.com>
2025-06-05 12:27:09 +0530

    Add files via upload

---
+++ LOTTERY NUMBER.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int usernum;
+    int luckynum=5;
+    
+    printf("Enter number between 1 to 10 : ");
+    scanf("%d",&usernum);
+    
+    if(usernum == luckynum)
+    {
+        printf("Winner");
+    }
+    else
+    {
+        printf("Looser");
+    }
+    getch();
+}
+    
